a382beb2e2455c4c01e3c678a157e04d598ba6d3
AdmiN <AdmiN@ITWS000>
2024-08-21 15:06:41 +0530

    Welcome

---
+++ src/lbrce/Welcome.java
@@ -0,0 +1,23 @@
+package lbrce;
+
+
+public class Welcome {
+
+
+
+	public static void main(String[] args)
+
+	{
+
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome 21761A0575");
+
+
+
+	}
+
+
+
+}
+
